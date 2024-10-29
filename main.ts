501ec1ab389eaaaeb2ae05e637fadb5f5fd1881b
Mary242524 <am.mzapatero25b@corazonistas.com>
2024-10-29 13:51:34 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,69 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    while (Enviado == 0) {
+        basic.pause(100)
+    }
+    if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (receivedNumber == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (receivedNumber == 3) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
+    basic.pause(2000)
+    Enviado = 0
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+    Enviado = 1
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+    Enviado = 1
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        # # . # #
+        # # . # #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+    Enviado = 1
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+let Enviado = 0
+radio.setGroup(1)
